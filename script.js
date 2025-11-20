dbbbf623d4bdcf4f0fdb18a21d5e42aac58ca1e2
CATALINA COHEN <49092736@ort.edu.ar>
2025-11-20 15:48:33 -0300

    va queriendooo ta lindito

--- script.js
+++ script.js
@@ -10,4 +10,25 @@ function abrirCupon() {
 
       // Cerrar al tocar fuera del cupón
       document.getElementById("overlayCupon").addEventListener("click", cerrarCupon);
-  
+  // MODAL RESTRICCIONES
+const modalRestriccion = document.getElementById('modalRestriccion');
+const openModalRestriccion = document.getElementById('openModalRestriccion');
+const closeModalRestriccion = document.getElementById('closeModalRestriccion');
+
+openModalRestriccion.onclick = () => modalRestriccion.classList.add('active');
+closeModalRestriccion.onclick = () => modalRestriccion.classList.remove('active');
+
+// MODAL CATEGORÍAS
+const modalCategoria = document.getElementById('modalCategoria');
+const openModalCategoria = document.getElementById('openModalCategoria');
+const closeModalCategoria = document.getElementById('closeModalCategoria');
+
+openModalCategoria.onclick = () => modalCategoria.classList.add('active');
+closeModalCategoria.onclick = () => modalCategoria.classList.remove('active');
+
+// Cerrar al tocar fuera (opcional)
+window.onclick = (e) => {
+  if (e.target.classList.contains('modal-restriccion')) {
+    e.target.classList.remove('active');
+  }
+}
